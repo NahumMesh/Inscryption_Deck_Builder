e7da2ccd3a9a8dedf50b22e4023a255843bb60f7
NahumMesh <115715631+NahumMesh@users.noreply.github.com>
2023-12-02 21:03:10 -0600

    Committing new files and getting rid of failed attempted files. This current version has a working bottom bar navigation system and basic skeleton login/signup function. All the screens are placeholder at the moment.

--- app/src/main/java/com/example/inscryptiondeckbuilder/MainActivity.kt
+++ app/src/main/java/com/example/inscryptiondeckbuilder/MainActivity.kt
@@ -5,18 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
 import com.example.inscryptiondeckbuilder.ui.theme.InscryptionDeckBuilderTheme
-import com.google.firebase.Firebase
-import com.google.firebase.firestore.firestore
+import com.example.inscryptiondeckbuilder.navgraphs.RootNavGraph
 
 class MainActivity : ComponentActivity() {
-    private val cardsCollection = Firebase.firestore.collection("cards")
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             InscryptionDeckBuilderTheme {
-                val navController = rememberNavController()
-                Nav(navController)
+                RootNavGraph(navController = rememberNavController())
             }
         }
     }
--- app/src/main/java/com/example/inscryptiondeckbuilder/SigilData.kt
+++ app/src/main/java/com/example/inscryptiondeckbuilder/SigilData.kt
@@ -1,7 +1,7 @@
 package com.example.inscryptiondeckbuilder
 
 data class SigilData(
-    val name:String?,
-    val effect:String?,
-    val imageFileName:String?
+    val name: String?,
+    val effect: String?,
+    val image_file_name: String?
 )

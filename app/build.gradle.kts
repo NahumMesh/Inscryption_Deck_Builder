f18e8d3beb07b9c82ac50d2762f5363ef6ae80a9
duckwheat <matthew.ccc17@gmail.com>
2023-12-15 21:11:19 -0600

    Fixed the unused implementations.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,12 +68,9 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore:24.10.0")
     implementation("com.google.firebase:firebase-storage:20.3.0")
     implementation("com.google.firebase:firebase-auth:22.3.0")
-    implementation("com.google.firebase:firebase-appcheck-playintegrity:17.1.1")
-    implementation("com.google.firebase:firebase-appcheck-debug:17.1.1")
     // Image loading using Coil.
     implementation("io.coil-kt:coil-compose:2.5.0")
     // Live data loading.
-    implementation("androidx.compose.runtime:runtime-livedata:1.6.0-beta02")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
